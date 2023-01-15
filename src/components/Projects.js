12e7e846d1ad97285125c7531c5ccd41ef814f5c
Abdullah Ahmad <52514546+AbdullahGumi@users.noreply.github.com>
2023-01-15 16:52:37 +0100

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -28,10 +28,10 @@ export default function Skills() {
 
   const Projects = [
     {
-      name: "NextTube",
-      desc: "Fullstack youtube clone built with nextjs",
-      tech: "Nextjs, TypeScript, Tailwindcss, MongoDB",
-      link: "https://next-tube-abdullahgumi.vercel.app/",
+      name: "Smart Box",
+      desc: "React app that uses AI to detect faces and apparels in an image",
+      tech: "React, Redux, Redux-thunk, Material UI, Clarifai",
+      link: "https://abdullahgumi.github.io/smart-box/",
     },
     {
       name: "Trello 2.0",
@@ -40,18 +40,13 @@ export default function Skills() {
       link: "https://trelloboard.onrender.com",
     },
     {
-      name: "Medium 2.0",
-      desc: "Nextjs Medium clone with ISR",
-      tech: "Typescript, Sanity CMS, Tailwindcss, Nextjs, React",
-      link: "https://medium2-0-rose.vercel.app/",
+      name: "NextTube",
+      desc: "Fullstack youtube clone built with nextjs",
+      tech: "Nextjs, TypeScript, Tailwindcss, MongoDB",
+      link: "https://next-tube-abdullahgumi.vercel.app/",
     },
-    {
-      name: "Smart Box",
-      desc: "React app that uses AI to detect faces and apparels in an image",
-      tech: "React, Redux, Redux-thunk, Material UI, Clarifai",
-      link: "https://abdullahgumi.github.io/smart-box/",
-    },
-    {
+    
+        {
       name: "Covid Tracker",
       desc: "Interactive map and charts that displays covid stats",
       tech: "React, Chart.js, react-simple-maps, Material UI, Styled-components",
@@ -69,38 +64,14 @@ export default function Skills() {
       tech: "React, Material UI, Styled-components, Google-maps",
       link: "https://material-fitness.vercel.app",
     },
-    {
-      name: "Profiles",
-      desc: "Mobile only React app made with tailwind",
-      tech: "React, redux-toolkit, react-router-dom, Tailwindcss, LocalStorage",
-      link: "https://profile-screens.vercel.app/profiles/?tab=user",
-    },
     // react native
     {
       name: "Video Player",
       desc: "Local videos player with react native",
       tech: "react-native, simple-thumbnail, react-native-video, ffmpeg",
       link: "https://github.com/AbdullahGumi/Player_RN",
-    },
-    {
-      name: "Uber2.0",
-      desc: "Uber clone with expo",
-      tech: "react-native, react-native-maps, expo, expo-location",
-      link: "https://github.com/AbdullahGumi/Uber2.0",
-    },
-    {
-      name: "Tinder2.0",
-      desc: "Tinder clone with expo",
-      tech: "react-native, firebase, tailwind, react-native-deck-swiper",
-      link: "https://github.com/AbdullahGumi/Tinder2.0",
-    },
-    {
-      name: "Deliveroo2.0",
-      desc: "Deliveroo food delivery service with expo",
-      tech: "react-native, redux, tailwind, Sanity CMS",
-      link: "https://github.com/AbdullahGumi/Deliveroo2.0",
-    },
-  ];
+    }
+];
 
   return (
     <Box className="container">
