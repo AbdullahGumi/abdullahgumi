bef47c5d059338828b330d3939398ec1d0be644b
AbdullahGumi <abdulgumi77@gmail.com>
2023-09-24 12:44:08 +0100

    updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -22,7 +22,8 @@ const items = [
     name: "AZA",
     details:
       "A fintech mobile application which enables users to perform P2P transactions, invest their money, and securely store funds in a vault for future needs",
-    image: AZAImage,
+    image:
+      "https://res.cloudinary.com/dv2csuzek/image/upload/v1695555675/aza_j5wton.jpg",
     site: "https://www.azanaija.com/",
     showCode: false,
     showSite: true,
@@ -33,7 +34,7 @@ const items = [
   //   name: "assets 360",
   //   details:
   //     "This is a sample of a project that I have completed for a client. The 360 Image Stitcher is a powerful app that allows users to create immersive 360-degree images using their mobile device's camera and motion sensors.",
-  //   image: Assets360Image,
+  //   image: "https://res.cloudinary.com/dv2csuzek/image/upload/v1695555672/assets360_wuv0cx.jpg",
   //   site: "https://getassets360.onrender.com/",
   //   showCode: false,
   //   showSite: true,
@@ -43,7 +44,8 @@ const items = [
     name: "Mintplex",
     details:
       "Mintplex is a platform designed to empower NFT creators with an innovative approach to minting and managing their digital assets.",
-    image: MintplexImage,
+    image:
+      "https://res.cloudinary.com/dv2csuzek/image/upload/v1695555676/mintplex_t2bhzc.jpg",
     site: "https://mintplex.xyz/",
     showCode: false,
     showSite: true,
@@ -52,7 +54,8 @@ const items = [
   {
     name: "Aperture",
     details: "A Cross-chain Investment Solution",
-    image: ApertureImage,
+    image:
+      "https://res.cloudinary.com/dv2csuzek/image/upload/v1695555675/aper_d6saa1.jpg",
     site: "https://aperture-ecru.vercel.app/",
     showCode: false,
     showSite: true,
@@ -61,7 +64,8 @@ const items = [
   {
     name: "Covid Tracker",
     details: "A react app to visualize covid stats",
-    image: CovidImage,
+    image:
+      "https://res.cloudinary.com/dv2csuzek/image/upload/v1695555675/covid_ek5zet.jpg",
     site: "https://covid-tracker-gilt-seven.vercel.app/",
     showCode: false,
     showSite: true,
@@ -70,7 +74,8 @@ const items = [
   {
     name: "Evolve Fitness",
     details: "A sample site designed for a client",
-    image: GymImage,
+    image:
+      "https://res.cloudinary.com/dv2csuzek/image/upload/v1695555675/gym_cpxnkq.jpg",
     site: "https://material-fitness.vercel.app/",
     showCode: false,
     showSite: true,
@@ -80,7 +85,8 @@ const items = [
     name: "Gee2",
     details:
       "A transportation mobile app that enables passengers to request rides while providing drivers with the ability to receive payments. It is mainly used In Enugu State of Nigeria.",
-    image: Gee2Image,
+    image:
+      "https://res.cloudinary.com/dv2csuzek/image/upload/v1695555676/gee2_pmaqkk.jpg",
     site: "https://gee2rides.com/",
     showCode: false,
     showSite: true,
@@ -90,7 +96,8 @@ const items = [
   {
     name: "Etijar",
     details: "A fintech mobile application for keeping track of loans",
-    image: EtijarImage,
+    image:
+      "https://res.cloudinary.com/dv2csuzek/image/upload/v1695555675/etijar_vglyyl.jpg",
     site: "https://www.etijar.com/",
     showCode: false,
     showSite: true,
@@ -100,7 +107,8 @@ const items = [
   {
     name: "Smart box",
     details: "React app that uses AI to detect faces and apparels in an image.",
-    image: SmartboxImage,
+    image:
+      "https://res.cloudinary.com/dv2csuzek/image/upload/v1695555675/smartbox_lnh9ol.jpg",
     code: "https://github.com/AbdullahGumi/smart-box",
     site: "https://abdullahgumi.github.io/smart-box/",
     showCode: true,
